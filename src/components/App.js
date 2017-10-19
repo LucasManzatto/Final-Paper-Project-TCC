eacc76b12e6e5042e8a484ac16b06839c50b8991
Lucas Manzatto Peres <lucasmanzatto21@gmail.com>
2017-10-19 10:36:57 -0200

    Change configs

---
+++ src/components/App.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+import AppBar from 'material-ui/AppBar';
+import './App.css';
+
+const AppBarExampleIcon = () => (
+  <AppBar
+    title="Title"
+    iconClassNameRight="muidocs-icon-navigation-expand-more"
+  />
+);
+export default App;
