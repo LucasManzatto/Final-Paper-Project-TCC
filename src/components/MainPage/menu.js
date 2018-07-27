3d01ae85d074e6d28fd057b10402a533e01d19e8
Lucas Manzatto <lucasmanzatto21@gmail.com>
2018-07-27 12:05:26 -0300

    .

--- src/components/MainPage/menu.js
+++ src/components/MainPage/menu.js
@@ -36,7 +36,7 @@ function Menu(props) {
   const { classes } = props;
   return (
     //<div className={classes.root}>
-    <MuiThemeProvider theme={theme}>
+    <MuiThemeProvider theme={theme} className={classes.root}>
       <AppBar position="static" color="primary">
         <Toolbar>
           <IconButton className={classes.menuButton} color="inherit" aria-label="Menu">
