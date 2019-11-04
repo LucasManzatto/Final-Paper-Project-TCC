a0babc0ef78a127743f7da64ea391d7b4ca9498b
Lucas Manzatto <lucas_peres21@hotmail.com>
2019-11-04 20:12:58 -0300

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/tcc/precache-manifest.65ecd06e72ecc7ee4799cbe5ce10c2d2.js"
+  "/tcc/precache-manifest.511528fbbf261a39b3983be5526c3329.js"
 );
 
 workbox.clientsClaim();
@@ -30,5 +30,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute("/tcc/index.html", {
   
-  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
